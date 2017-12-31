f5952c25278d977268cae4ea4c55e2b942f5058d
ychalier <yohan.chalier@telecom-paristech.fr>
2017-12-31 13:45:34 +0100

    cleanup & comments

--- src/server/CommandHandler.java
+++ src/server/CommandHandler.java
@@ -1,5 +1,12 @@
 package server;
 
+/**
+ * Used by the user input to
+ * handle commands.
+ * 
+ * @author Yohan Chalier
+ *
+ */
 public interface CommandHandler {
 	
 	String executeCommand(String cmd);
--- src/server/UserInput.java
+++ src/server/UserInput.java
@@ -2,6 +2,13 @@ package server;
 
 import java.util.Scanner;
 
+/**
+ * A thread to read user input and execute commands.
+ * Server-side.
+ * 
+ * @author Yohan Chalier
+ *
+ */
 public class UserInput extends Thread {
 	
 	private CommandHandler cmdHandler;
@@ -14,9 +21,15 @@ public class UserInput extends Thread {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print(">");
+		
 		while (sc.hasNextLine()) {
+			// Reading command
 			String cmd = sc.nextLine();
+			
+			// Executing command and storing output
 			String str = cmdHandler.executeCommand(cmd);
+			
+			// Close thread on null response
 			if (str == null) {
 				break;
 			} else {
