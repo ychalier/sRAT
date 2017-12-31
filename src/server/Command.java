5942d1abda5bd3c0eb01630094ea56f0f77d6917
ychalier <yohan.chalier@telecom-paristech.fr>
2017-12-31 23:01:22 +0100

    comment

--- src/server/Command.java
+++ src/server/Command.java
@@ -1,8 +1,10 @@
 package server;
 
 /**
- * Represents a command to be executed
- * by the control server.
+ * Represents a command to be executed by the command and control server.
+ * 
+ * Arguments are passed through an array of strings, and an output
+ * string is returned.
  * 
  * @author Yohan Chalier
  *
