4a1180afd0bfbb3eaa30a8bea47f99d22453bdbf
ychalier <yohan.chalier@telecom-paristech.fr>
2018-01-01 12:22:29 +0100

    add client

---
+++ src/client/Client.java
@@ -0,0 +1,15 @@
+package client;
+
+public class Client extends Thread {
+	
+	private static final String SERVER_DOMAIN = "rat.chalier.fr";
+	
+	@Override
+	public void run(){
+		while (true) {
+			// TODO 
+		}
+	}
+	
+
+}
